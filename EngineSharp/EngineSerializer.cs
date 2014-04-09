d5f58644a308721347702fdbbc53344cd2d5fa1e
ZA1 <geoff@bfound.co.za>
2014-04-09 08:25:04 +0200

    - Added missing file

---
+++ EngineSharp/EngineSerializer.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using RestSharp.Serializers;
+
+namespace EngineSample
+{
+    public class EngineSerializer : ISerializer
+    {
+        public EngineSerializer()
+        {
+            ContentType = "application/json";
+        }
+
+        public string Serialize(object obj)
+        {
+            return JsonConvert.SerializeObject(obj, new JsonSerializerSettings
+            {
+                ContractResolver = new EngineMappingResolver(),
+                NullValueHandling = NullValueHandling.Ignore
+            });
+        }
+
+        public string RootElement { get; set; }
+        public string Namespace { get; set; }
+        public string DateFormat { get; set; }
+        public string ContentType { get; set; }
+    }
+}
